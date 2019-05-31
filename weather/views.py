f13cdcea129085b0f01c2f136f1a813cf9dac967
James Cho <hwisoocho@gmail.com>
2019-05-30 22:38:35 -0700

    form for user input

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,10 @@
 import os
 import requests
-from dotenv import read_dotenv
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+from dotenv import read_dotenv
 
 read_dotenv('.ENV')
 WEATHER_API_KEY = (os.environ.get('WEATHER_API_KEY'))
@@ -9,14 +12,29 @@ WEATHER_API_KEY = (os.environ.get('WEATHER_API_KEY'))
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
     city = 'Houston'
-    r = requests.get(url.format(city, WEATHER_API_KEY)).json()
 
-    city_weather = {
-      'city': city,
-      'temperature': r['main']['temp'],
-      'description': r['weather'][0]['description'],
-      'icon': r['weather'][0]['icon'],
-    }
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    context = {'city_weather' : city_weather}
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+    
+    for city in cities:
+        r = requests.get(url.format(city, WEATHER_API_KEY)).json()
+
+        city_weather = {
+          'city': city.name,
+          'temperature': r['main']['temp'],
+          'description': r['weather'][0]['description'].title(),
+          'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+
+    context = {'weather_data' : weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
