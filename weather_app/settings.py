2f59c1e7c5777b0bdeea0ecd02755008df2a2aa8
James Cho <hwisoocho@gmail.com>
2019-06-05 19:46:47 -0700

    configuring for heroku

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r!3*!#*ejac5!)-or3r3$t^7qvnl13v24*q2o=&yk&*^+ab4uc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
